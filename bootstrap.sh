72c37a8d11fbf8a644c5dd0bd978978eeb1366ec
Stephen Bourget <sbourget93@gmail.com>
2019-03-22 19:57:49 -0700

    permission

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,10 @@
 yum install -y java-1.8.0
 yum remove -y java-1.7.0-openjdk.x86_64
 
+chown -R ec2-user /home/ec2-user
+chmod -R g+rwx /home/ec2-user
+chmod -R g+s /home/ec2-user
+
 cd /
 easy_install supervisor
 /usr/local/bin/supervisord -c /home/ec2-user/minecraft/supervisord.conf
-
-chmod -R 777 /home/ec2-user/minecraft
