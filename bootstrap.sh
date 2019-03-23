fd92d69f8c9e3474af01c7d79dc56a0279295076
Stephen Bourget <sbourget93@gmail.com>
2019-03-22 18:23:25 -0700

    supervisor to run server

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,10 @@
+# yum is updated in user-data script
+
 yum install -y java-1.8.0
 yum remove -y java-1.7.0-openjdk.x86_64
+
+cd /
+easy_install supervisor
+/usr/local/bin/supervisord -c /home/ec2-user/minecraft/supervisord.conf
+
+chmod -R 777 /home/ec2-user/minecraft
--- run_forge.sh
+++ run_forge.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cd minecraft
 java -Xmx6G -Xms6G -jar forge-1.12.2-14.23.3.2655-universal.jar nogui
