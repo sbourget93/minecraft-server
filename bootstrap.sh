692fa5a9bf46ca781c0b13e0936c395ca22c5999
Stephen Bourget <sbourget93@gmail.com>
2019-03-23 11:40:23 -0700

    adjust permissions on instance

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,9 @@
 yum install -y java-1.8.0
 yum remove -y java-1.7.0-openjdk.x86_64
 
-chown -R ec2-user /home/ec2-user
-chmod -R g+rwx /home/ec2-user
-chmod -R g+s /home/ec2-user
+chown -R ec2-user /home/ec2-user/minecraft
+chmod -R g+rwx /home/ec2-user/minecraft
+chmod -R g+s /home/ec2-user/minecraft
 
-cd /
 easy_install supervisor
 /usr/local/bin/supervisord -c /home/ec2-user/minecraft/supervisord.conf
