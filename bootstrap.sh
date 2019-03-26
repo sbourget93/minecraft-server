6414dc95164fd2b4bd36990520f6996c42bcbaf4
Stephen Bourget <sbourget93@gmail.com>
2019-03-26 10:49:18 -0700

    write directly to crontab

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ chown -R ec2-user /home/ec2-user/minecraft
 chmod -R g+rwx /home/ec2-user/minecraft
 chmod -R g+s /home/ec2-user/minecraft
 
-crontab /home/ec2-user/minecraft/crontab.config
+echo '30 * * * * python /home/ec2-user/minecraft/copy_latest_backup.py'  >> /var/spool/cron/ec2-user
 
 easy_install supervisor
 /usr/local/bin/supervisord -c /home/ec2-user/minecraft/supervisord.conf
