62bcdbdd745372ff814ba03e275d7dde97b2a7bb
Stephen Bourget <sbourget93@gmail.com>
2019-05-05 21:09:49 -0700

    eula, config

--- backup_to_s3.py
+++ backup_to_s3.py
@@ -1,11 +1,6 @@
 import boto3
-import os
-
-from datetime import datetime
 
 if __name__ == '__main__':
-
-    most_recent_file = None
 
     with open('/tmp/server_name') as f:
         server_name = f.readline().strip()
@@ -14,26 +9,6 @@ if __name__ == '__main__':
     bucket = 'stephengb-minecraft'
     prefix = '%s/backups/' % server_name
     backup_format = 'Backup-world-%Y-%m-%d--%H-%M.zip'
-
-    for root, directories, file_names in os.walk(path):
-        for file_name in file_names:
-            file_path = os.path.join(root, file_name)
-            try:
-                file_date = datetime.strptime(file_name, backup_format)
-                if most_recent_file is None:
-                    most_recent_file = {
-                        'file_path': file_path,
-                        'file_name': file_name,
-                        'file_date': file_date
-                    }
-                elif file_date > most_recent_file['file_date']:
-                    most_recent_file = {
-                        'file_path': file_path,
-                        'file_name': file_name,
-                        'file_date': file_date
-                    }
-            except Exception as e:
-                pass
 
     if most_recent_file is not None:
 
--- run_minecraft.sh
+++ run_minecraft.sh
@@ -4,4 +4,4 @@ echo $(date +%Y-%m-%dT%H:%M:%S) > /tmp/server_started_at
 
 cd "$(dirname "$0")"
 cd minecraft
-java -Xmx6G -Xms6G -jar minecraft_server.1.14.jar nogui
+java -Xmx5G -Xms5G -jar minecraft_server.1.14.jar nogui
