4d857a180e05fafbe7259055422fd2f20d73ca22
Stephen Bourget <sbourget93@gmail.com>
2020-04-03 19:59:37 -0700

    change minecraft version

--- run_forge.sh
+++ run_forge.sh
@@ -4,4 +4,4 @@ echo $(date +%Y-%m-%dT%H:%M:%S) > /tmp/server_started_at
 
 cd "$(dirname "$0")"
 cd minecraft
-java -Xmx6G -Xms6G -jar forge-1.7.10-10.13.4.1558-1.7.10-universal.jar nogui
+java -Xmx6G -Xms6G -jar forge-1.15.2-31.1.35.jar nogui
