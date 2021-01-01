9c4c4ada34ce89c3c9fc5b49727346659a2c48a9
Stephen Bourget <sbourget93@gmail.com>
2021-01-01 00:59:22 -0500

    add fml confirm

--- run_forge.sh
+++ run_forge.sh
@@ -4,4 +4,4 @@ echo $(date +%Y-%m-%dT%H:%M:%S) > /tmp/server_started_at
 
 cd "$(dirname "$0")"
 cd minecraft
-java -Xmx13G -Xms13G -jar forge-1.12.2-14.23.5.2854.jar nogui
+java -Xmx13G -Xms13G -jar forge-1.12.2-14.23.5.2854.jar nogui -Dfml.queryResult=confirm
