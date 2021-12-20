dae97993bbdc8d8a7fe08ade27db253da564ec6e
Stephen Bourget <sbourget93@gmail.com>
2021-12-20 12:18:56 -0500

    skeleton

--- run_forge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo $(date +%Y-%m-%dT%H:%M:%S) > /tmp/server_started_at
-
-cd "$(dirname "$0")"
-cd minecraft
-java -Xmx6G -Xms6G -jar forge-1.7.10-10.13.4.1558-1.7.10-universal.jar nogui
