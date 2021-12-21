df7a64c7b1b8f5dcc1f7e2a8aa43c2c128c12df1
Stephen Bourget <sbourget93@gmail.com>
2021-12-21 08:46:03 -0500

    update run script

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo $(date +%Y-%m-%dT%H:%M:%S) > /tmp/server_started_at
-
-cd "$(dirname "$0")"
 cd /home/ec2-user/minecraft-server/minecraft/
 java -Xmx5G -Xms5G -jar server.jar nogui
