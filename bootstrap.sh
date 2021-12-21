30b8c41447c71ca904e00bfac5a46f03a186445e
Stephen Bourget <sbourget93@gmail.com>
2021-12-21 08:59:04 -0500

    add supervisor file

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,20 @@ server_name=$1
 # install java 17 which is needed for Minecraft 1.18
 wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
 rpm -Uvh jdk-17_linux-x64_bin.rpm
+rm jdk-17_linux-x64_bin.rpm
 
-#pip install boto3==1.9.121
+pip3 install boto3==1.20.25
 
 #cd /home/ec2-user/minecraft-server/minecraft
 #aws s3 cp s3://stephengb-minecraft/$server_name/backups/latest-backup.zip . || true
 #unzip latest-backup.zip || true
 #rm latest-backup.zip
 
-#chown -R ec2-user /home/ec2-user/minecraft-server
-#chmod -R g+rwx /home/ec2-user/minecraft-server
-#chmod -R g+s /home/ec2-user/minecraft-server
+chown -R ec2-user /home/ec2-user/minecraft-server
+chmod -R g+rwx /home/ec2-user/minecraft-server
+chmod -R g+s /home/ec2-user/minecraft-server
 
-#echo '30 * * * * python /home/ec2-user/minecraft-server/copy_latest_backup.py'  >> /var/spool/cron/ec2-user
+echo '30 * * * * python3 /home/ec2-user/minecraft-server/copy_latest_backup.py'  >> /var/spool/cron/ec2-user
 
-#easy_install supervisor
-#/usr/local/bin/supervisord -c /home/ec2-user/minecraft-server/supervisord.conf
+easy_install supervisor
+/usr/local/bin/supervisord -c /home/ec2-user/minecraft-server/supervisord.conf
