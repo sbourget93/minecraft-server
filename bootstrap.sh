2cef21c3445061fb5918c733b0a81c148b46a7b9
Stephen Bourget <sbourget93@gmail.com>
2021-12-21 10:50:41 -0500

    better file permissions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,27 @@
 server_name=$1
 
-#mkdir /home/ec2-user/minecraft-server/backups
+mkdir /home/ec2-user/minecraft-server/backups
 
 # install java 17 which is needed for Minecraft 1.18
-#wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
-#rpm -Uvh jdk-17_linux-x64_bin.rpm
-#rm jdk-17_linux-x64_bin.rpm
+wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
+rpm -Uvh jdk-17_linux-x64_bin.rpm
+rm jdk-17_linux-x64_bin.rpm
 
-#pip3 install boto3==1.20.25
+pip3 install boto3==1.20.25
 
 #cd /home/ec2-user/minecraft-server/minecraft
 #aws s3 cp s3://stephengb-minecraft/$server_name/backups/latest-backup.zip . || true
 #unzip latest-backup.zip || true
 #rm latest-backup.zip
 
-#chown -R ec2-user /home/ec2-user/minecraft-server
-#chmod -R g+rwx /home/ec2-user/minecraft-server
-#chmod -R g+s /home/ec2-user/minecraft-server
-#chmod -R 777 /home/ec2-user/minecraft-server
+# make ec2-user owner of everything in the minecraft-server directory
+chown -R ec2-user /home/ec2-user/minecraft-server
+chmod -R 600 /home/ec2-user/minecraft-server
+chmod +x /home/ec2-user/minecraft-server/backup_world.sh
+chmod +x /home/ec2-user/minecraft-server/run_server.sh
 
+echo '*/5 * * * * /home/ec2-user/minecraft-server/backup_world.sh' >> /var/spool/cron/ec2-user
 
-#echo '*/5 * * * * /home/ec2-user/minecraft-server/backup_world.sh' >> /var/spool/cron/ec2-user
-
-#easy_install supervisor
-#/usr/bin/supervisord -c /home/ec2-user/minecraft-server/supervisord.conf
+easy_install supervisor
+# supervisor runs 'run_server.sh' as ec2-user
+/usr/bin/supervisord -c /home/ec2-user/minecraft-server/supervisord.conf
