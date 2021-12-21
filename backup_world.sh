59c30a8dd75f878d2e2d2f73c12a07f9b063a88b
Stephen Bourget <sbourget93@gmail.com>
2021-12-20 20:23:39 -0500

    add backup script

---
+++ backup_world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/ec2-user/minecraft-server/server_files/minecraft/
+zip -r /home/ec2-user/minecraft-server/backups/Backup-world-`date +%Y-%m-%d--%H-%M.zip`  world
+
+find /home/ec2-user/minecraft-server/backups/* -mmin +60 -delete
