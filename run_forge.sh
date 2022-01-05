6896c1002fbcf820e005a8c9f45e79f84c44b8f4
Stephen Bourget <sbourget93@gmail.com>
2022-01-05 12:20:02 -0500

    changes

--- run_forge.sh
+++ run_forge.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+SERVER_RAM=`cat /usr/etc/server_ram`
+
 cd /home/ec2-user/minecraft-server/minecraft
 java -Xmx${SERVER_RAM}G -Xms${SERVER_RAM}G -jar forge-1.16.5-36.2.20.jar nogui -Dfml.queryResult=confirm
